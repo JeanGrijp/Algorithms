dbf5cd640588251add295359356d75b13ed8183f
Jean Grijp <grijpjean@gmail.com>
2020-09-09 03:52:25 -0300

    .

--- Listas 2020.3/Lista 2/Q1.py
+++ Listas 2020.3/Lista 2/Q1.py
@@ -46,7 +46,7 @@ class BinarySearchTree:
         depth = 0
         while no is not None:
             if no.key == key:
-                return no.key, depth
+                return depth
             elif no.key < key:
                 no = no.right
                 depth += 1
@@ -54,6 +54,7 @@ class BinarySearchTree:
                 no = no.left
                 depth += 1
         return -1
+
 
     def remove(self, key):
         '''
@@ -218,11 +219,71 @@ class BinarySearchTree:
 
 
 inp = ['100', '570 250 0 220 60 40 10 20 30 50 200 80 70 170 150 140 110 100 90 120 130 160 190 180 210 230 240 280 260 270 310 300 290 450 380 360 350 320 330 340 370 390 420 400 410 440 430 470 460 520 490 480 500 510 550 530 540 560 940 760 630 590 580 610 600 620 650 640 700 690 670 660 680 720 710 730 740 750 860 840 770 780 790 830 800 820 810 850 870 890 880 930 900 910 920 980 970 950 960 990', 'SCH 1008', 'SCH 280', 'SCH 100', 'SCH 1006', 'SCH 200', 'SCH 490', 'SCH 210', 'SCH 480', 'SCH 700', 'SCH 930', 'SCH 940', 'SCH 1001', 'SCH 1002', 'SCH 1005', 'SCH 790', 'SCH 1004', 'SCH 990', 'SCH 820', 'SCH 1008', 'SCH 880', 'SCH 120', 'SCH 1003', 'SCH 1000', 'SCH 880', 'SCH 70', 'SCH 200', 'SCH 220', 'SCH 340', 'SCH 800', 'SCH 170', 'SCH 120', 'SCH 620', 'SCH 1005', 'SCH 750', 'SCH 1008', 'SCH 390', 'SCH 1001', 'SCH 220', 'SCH 1006', 'SCH 560', 'SCH 350', 'SCH 1005', 'SCH 1000', 'SCH 830', 'SCH 800', 'SCH 1001', 'SCH 1000', 'SCH 1002', 'SCH 1007', 'SCH 210', 'SCH 270', 'SCH 1004', 'SCH 410', 'SCH 1001', 'SCH 1004', 'SCH 500', 'SCH 1005', 'SCH 540', 'SCH 300', 'SCH 150', 'SCH 1003', 'SCH 1009', 'SCH 1002', 'SCH 530', 'SCH 1006', 'SCH 170', 'SCH 840', 'SCH 1008', 'SCH 1007', 'SCH 680', 'SCH 1003', 'SCH 1009', 'SCH 860', 'SCH 20', 'SCH 470', 'SCH 1004', 'SCH 600', 'SCH 1000', 'SCH 530', 'SCH 1003', 'SCH 1000', 'SCH 1005', 'SCH 1005', 'SCH 930', 'SCH 1007', 'SCH 390', 'SCH 460', 'SCH 610', 'SCH 1000', 'SCH 1006', 'SCH 1007', 'SCH 1003', 'SCH 410', 'SCH 390', 'SCH 1008', 'SCH 870', 'SCH 1001', 'SCH 1007', 'SCH 1001', 'SCH 1006', 'END']
+
+
 aux = inp[1].split(" ")
 num = inp[0]
-
+lista = inp[2::]
 
 arvore = BinarySearchTree()
 
 for i in aux:
     arvore.insert(int(i), i)
+
+
+maior = 0
+for i in aux:
+    num = arvore.search(int(i))
+    if maior < num:
+        maior = num
+print(maior)
+
+for i in lista:
+    if i[:3:] == "SCH":
+        print((arvore.search(int(i[4::]))))
+    elif i[:3:] == "INS":
+        arvore.insert(int(i[4::]), i)
+        print((arvore.search(int(i[4::]))))
+    elif i[:3:] == "DEL":
+        arvore.remove(int(i[4::]))
+        print((arvore.search(int(i[4::]))))
+
+
+def main():
+    inputs = []
+    cont = True
+    try:
+        while cont:
+            inputs.append(input())
+    except:
+        pass
+    
+    aux = inputs[1].split(" ")
+    num = inputs[0]
+    lista = inp[2::]
+
+    arvore = BinarySearchTree()
+
+    for i in aux:
+        arvore.insert(int(i), i)
+
+
+    maior = 0
+    for i in aux:
+        num = arvore.search(int(i))
+        if maior < num:
+            maior = num
+    print(maior)
+
+    for i in lista:
+        if i[:3:] == "SCH":
+            print((arvore.search(int(i[4::]))))
+        elif i[:3:] == "INS":
+            arvore.insert(int(i[4::]), i)
+            print((arvore.search(int(i[4::]))))
+        elif i[:3:] == "DEL":
+            arvore.remove(int(i[4::]))
+            print((arvore.search(int(i[4::]))))
+
+if __name__ == '__main__':
+    main()
