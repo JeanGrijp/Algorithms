109d5c79fcfdf5e836fd9ae9f69fba097fc6d4b7
Jean Grijp <grijpjean@gmail.com>
2020-09-09 00:51:54 -0300

    .

--- Listas 2020.3/Lista 2/Q1.py
+++ Listas 2020.3/Lista 2/Q1.py
@@ -15,6 +15,19 @@ class BinarySearchTree:
     def __len__(self):
         return self.size
 
+    def search(self, key):
+        no = self.root
+        depth = 0
+        while no is not None:
+            if no.key == key:
+                return no.value, depth
+            elif key > no.key:
+                no = no.right
+                depth += 1
+            else:
+                no = no.left
+                depth += 1
+
     def insert(self, key, value):
         cont = True
         no_current = self.root
@@ -23,6 +36,7 @@ class BinarySearchTree:
             while cont:
                 if no_current is None:
                     self.root = No(key, value)
+                    print(self.search(key)[1])
                     self.size += 1
                     cont = False
                 elif key < no_current.key:
@@ -39,19 +53,6 @@ class BinarySearchTree:
                         no_before.right = No(key, value)
                         self.size += 1
                         cont = False
-
-    def search(self, key):
-        no = self.root
-        depth = 0
-        while no is not None:
-            if no.key == key:
-                return no.value, depth
-            elif key > no.key:
-                no = no.right
-                depth += 1
-            else:
-                no = no.left
-                depth += 1
 
     def remove(self, key):
         '''
@@ -76,6 +77,12 @@ class BinarySearchTree:
         while no_current is not None:
             # verifica se encontrou o nó a ser removido
             if no_current.key == key:
+
+
+
+
+
+
                 # caso 1: o nó a ser removido não possui filhos (nó folha)
                 if no_current.left is None and no_current.right is None:
                     # verifica se é a root
@@ -91,6 +98,12 @@ class BinarySearchTree:
                         elif no_before.right == no_current:
                             no_before.right = None
                             self.size -= 1
+
+
+
+
+
+
                 # caso 2: o nó a ser removido possui somente um filho
                 elif (no_current.left is None and no_current.right is not None) or (no_current.left is not None and no_current.right is None):
                     # verifica se o nó a ser removido é a root
@@ -116,7 +129,12 @@ class BinarySearchTree:
                                 no_before.left = no_current.right
                             else:# senão no_current é filho à right
                                 no_before.right = no_current.right
-                    
+
+
+
+
+
+
                 # caso 3: o nó a ser removido possui dois filhos
                 # pega-se o menor elemento da subárvore à direita
                 elif (no_current.left is not None) and (no_current.right is not None):
@@ -178,3 +196,8 @@ class BinarySearchTree:
             else:
                 no_current = no_current.right
         return no_current.value
+
+
+
+inp = ['100', '570 250 0 220 60 40 10 20 30 50 200 80 70 170 150 140 110 100 90 120 130 160 190 180 210 230 240 280 260 270 310 300 290 450 380 360 350 320 330 340 370 390 420 400 410 440 430 470 460 520 490 480 500 510 550 530 540 560 940 760 630 590 580 610 600 620 650 640 700 690 670 660 680 720 710 730 740 750 860 840 770 780 790 830 800 820 810 850 870 890 880 930 900 910 920 980 970 950 960 990', 'SCH 1008', 'SCH 280', 'SCH 100', 'SCH 1006', 'SCH 200', 'SCH 490', 'SCH 210', 'SCH 480', 'SCH 700', 'SCH 930', 'SCH 940', 'SCH 1001', 'SCH 1002', 'SCH 1005', 'SCH 790', 'SCH 1004', 'SCH 990', 'SCH 820', 'SCH 1008', 'SCH 880', 'SCH 120', 'SCH 1003', 'SCH 1000', 'SCH 880', 'SCH 70', 'SCH 200', 'SCH 220', 'SCH 340', 'SCH 800', 'SCH 170', 'SCH 120', 'SCH 620', 'SCH 1005', 'SCH 750', 'SCH 1008', 'SCH 390', 'SCH 1001', 'SCH 220', 'SCH 1006', 'SCH 560', 'SCH 350', 'SCH 1005', 'SCH 1000', 'SCH 830', 'SCH 800', 'SCH 1001', 'SCH 1000', 'SCH 1002', 'SCH 1007', 'SCH 210', 'SCH 270', 'SCH 1004', 'SCH 410', 'SCH 1001', 'SCH 1004', 'SCH 500', 'SCH 1005', 'SCH 540', 'SCH 300', 'SCH 150', 'SCH 1003', 'SCH 1009', 'SCH 1002', 'SCH 530', 'SCH 1006', 'SCH 170', 'SCH 840', 'SCH 1008', 'SCH 1007', 'SCH 680', 'SCH 1003', 'SCH 1009', 'SCH 860', 'SCH 20', 'SCH 470', 'SCH 1004', 'SCH 600', 'SCH 1000', 'SCH 530', 'SCH 1003', 'SCH 1000', 'SCH 1005', 'SCH 1005', 'SCH 930', 'SCH 1007', 'SCH 390', 'SCH 460', 'SCH 610', 'SCH 1000', 'SCH 1006', 'SCH 1007', 'SCH 1003', 'SCH 410', 'SCH 390', 'SCH 1008', 'SCH 870', 'SCH 1001', 'SCH 1007', 'SCH 1001', 'SCH 1006', 'END']
+aux = inp[1].split(" ")
