40a21b614882960085be2d9712850f91b1f43d59
Jean Grijp <grijpjean@gmail.com>
2020-09-10 18:42:28 -0300

    .

--- Listas 2020.3/Lista 2/Q1.py
+++ Listas 2020.3/Lista 2/Q1.py
@@ -69,7 +69,7 @@ class BinarySearchTree:
                     no_current = no_current.left
                     if no_current is None:
                         no_before.left = No(key, value)
-                        self.depth.append(depth)
+                        self.depth.append((key, depth))
                         self.size += 1
                         cont = False
                 elif key >= no_current.key:
@@ -78,9 +78,132 @@ class BinarySearchTree:
                     no_current = no_current.right
                     if no_current is None: 
                         no_before.right = No(key, value)
-                        self.depth.append(depth)
+                        self.depth.append((key, depth))
                         self.size += 1
                         cont = False
+    
+
+    def remover(self, key):
+        self.root, value = self.__remover(self.root, key)
+        return value
+    
+    def __remover(self, no, key):
+        if no is None:
+            raise KeyError(key)
+        elif key > no.key:
+            no.right, value = self.__remover(no.right, key)
+        elif key < no.key:
+            no.left, value = self.__remover(no.left, key)
+        else:
+            value = no.value
+            if no.right is None:
+                aux = no
+                no = no.left
+                del aux
+            elif no.left is None:
+                aux = no
+                no = no.right
+                del aux
+            else:
+                no.right = self.__sucessor(no, no.right)
+        return no, value
+    
+    def __sucessor(self, no, nodo):
+        '''
+        Para promover um nó é necessário achar um entre os dois value possíveis
+        para a substituição: o nó imediatamente anterior ao nó que vai ser
+        substituído (antecessor) ou o imediatamente posterior (sucessor). Para
+        essa implementação eu escolhi fazer a substituição pelo sucessor, mas
+        a implementação com o antecessor pode ser vista nos slides do professor
+        Renato.
+        O antecessor é sempre o maior entre os menores (o mais a direita entre
+        os nós a esquerda do nó que será substituído) e o sucessor é o menor
+        entre os maiores (o mais a esquerda dos nós a direita).
+        '''
+        if nodo.left is not None:
+            nodo.left = self.__sucessor(no, nodo.left)
+        else:
+            aux = nodo
+            no.value = nodo.value
+            no.key = nodo.key
+            nodo = nodo.right
+            del aux
+        return nodo
+
+    
+    def remove___(self, key):
+        key = int(key)
+        no_current = self.root
+        no_before = None
+        while no_current is not None:
+            if no_current.key == key:
+                if no_current.left is None and no_current.right is None:
+                    if no_before is None:
+                        self.root = None
+                        self.size = 0
+                    else:
+                        if no_before.left == no_current:
+                            no_before.left = None
+                            self.size -= 1
+                        elif no_before.right == no_current:
+                            no_before.right = None
+                            self.size -= 1
+                elif (no_current.left is None and no_current.right is not None) or (no_current.left is not None and no_current.right is None):
+                    if no_before is None:
+                        if no_current.left is not None:
+                            self.root = no_current.left
+                            self.size -= 1
+                        else:
+                            self.root = no_current.right
+                            self.size -= 1
+                    else:
+                        if no_current.left is not None:
+                            if no_before.left == no_current:
+                                no_before.left = no_current.left
+                            else:
+                                no_before.right = no_current.left
+                        else:
+                            if no_before.left == no_current:
+                                no_before.left = no_current.right
+                            else:
+                                no_before.right = no_current.right
+                elif (no_current.left is not None) and (no_current.right is not None):
+                    menor_no_before = no_current
+                    menor_no = no_current.right
+                    proximo_menor = no_current.right.left               
+                    while proximo_menor is not None:
+                        menor_no_before = menor_no
+                        menor_no = proximo_menor
+                        proximo_menor = menor_no.left
+                    if no_before is None:
+                        if self.root.right.key == menor_no.key:
+                            menor_no.left = self.root.left
+                        else:
+                            if menor_no_before.left.key == menor_no.key:
+                                menor_no_before.left = None
+                            else:
+                                menor_no_before.right = None
+                            menor_no.left = no_current.left
+                            menor_no.right = no_current.right
+                        self.root = menor_no
+                    else:
+                        if no_before.left.key == no_current.key:
+                            no_before.left = menor_no
+                        else:
+                            no_before.right = menor_no
+                        if menor_no_before.left.key == menor_no.key:
+                            menor_no_before.left = None
+                        else:
+                            menor_no_before.right = None
+                        menor_no.left = no_current.left
+                        menor_no.right = no_current.right
+                break 
+            no_before = no_current
+            if key < no_current.key:
+                no_current = no_current.left
+            else:
+                no_current = no_current.right
+        return no_current.key
 
     def search(self, key):
         no = self.root
@@ -98,162 +221,62 @@ class BinarySearchTree:
                 depth += 1
         return -1
 
-    def deepSearch(self):
-        biggest = 0
-        for i in self.depth:
-            if i > biggest:
-                biggest = i
-        return biggest
 
+
+
+
+    def produndidade(self, no):
+        if no == None or no.left == None and no.right == None:
+            return 1
+        else:
+            if self.produndidade(no.left) > self.produndidade(no.right):
+                return  1 + self.produndidade(no.left) 
+            else:
+                return  1 + self.produndidade(no.right) 
+
+
+    def height(self, no):
+        if no == None or no.left == None and no.right == None:
+            return 1
+        else:
+            if self.height(no.left) > self.height(no.right):
+                return  1 + self.height(no.left) 
+            else:
+                return  1 + self.height(no.right) 
 
 
 
  
-    def remove(self, key):
-        
-        key = int(key)
-        no_current = self.root
-        no_before = None
-        while no_current is not None:
-
-            if no_current.key == key:
-                if no_current.left is None and no_current.right is None:
-                    if no_before is None:
-                        self.root = None
-                        self.size = 0
-                    else:
-
-                        if no_before.left == no_current:
-
-                            no_before.left = None
-                            self.size -= 1
-
-                        elif no_before.right == no_current:
-
-                            no_before.right = None
-                            self.size -= 1
 
 
 
+inp = ["", "46 101 10 2 90 220 148 30 20 40 128 374 253 128 34 451 194 448 919 527 451"]
 
-
-
-
-                elif (no_current.left is None and no_current.right is not None) or (no_current.left is not None and no_current.right is None):
-
-                    if no_before is None:
-                        if no_current.left is not None:
-
-                            self.root = no_current.left
-                            self.size -= 1
-                        else:
-                            self.root = no_current.right
-                            self.size -= 1
-                    else:
-
-                        if no_current.left is not None:
-
-                            if no_before.left == no_current:
-
-                                no_before.left = no_current.left
-                            else:
-                                no_before.right = no_current.left
-                        else:
-                            if no_before.left == no_current:
-                                no_before.left = no_current.right
-                            else:
-                                no_before.right = no_current.right
-
-                elif (no_current.left is not None) and (no_current.right is not None):
-                    
-                    menor_no_before = no_current
-                    menor_no = no_current.right
-                    proximo_menor = no_current.right.left               
-                    while proximo_menor is not None:
-                        menor_no_before = menor_no
-                        menor_no = proximo_menor
-                        proximo_menor = menor_no.left
-                    if no_before is None:
-                        if self.root.right.key == menor_no.key:
-                            menor_no.left = self.root.left
-                        else:
-                            if menor_no_before.left.key == menor_no.key:
-                                menor_no_before.left = None
-                            else:
-
-                                menor_no_before.right = None
-
-                            menor_no.left = no_current.left
-                            menor_no.right = no_current.right
-
-                        self.root = menor_no
-                    else:
-                        if no_before.left.key == no_current.key:
-
-                            no_before.left = menor_no
-                        else:
-
-                            no_before.right = menor_no
-                        if menor_no_before.left.key == menor_no.key:
-
-                            menor_no_before.left = None
-                        else:
-
-                            menor_no_before.right = None
-
-
-                        menor_no.left = no_current.left
-                        menor_no.right = no_current.right
-                break 
-            no_before = no_current
-
-
-            if key < no_current.key:
-                no_current = no_current.left
-            else:
-                no_current = no_current.right
-        return no_current.key
-
-
-
-inp = ["", "46 100 10 2 90 220 148 30 20 40 128 374 253 128 34 451 194 448 919 527 451"]
-
-comands = ['INS 71', 'DEL 148', 'SCH 451']
+comands = ['INS 71', 'DEL 30', 'SCH 20']
 
 
 
 aux = inp[1].split(" ")
-# print(aux)
-
-
 arvore = BinarySearchTree()
-
 for i in aux:
-    print(i+" -> ")
-    arvore.insertAndPrint(int(i), i[4::])
-    print("######################################################")
+    # print(i + "->")
+    arvore.insert(int(i), i[4::])
 
 
 
 
-print(arvore.deepSearch())
-
-#     # if i == "100":
-#     #     print(arvore.search(int(i)))
-# print("######################################################")
-
-# maior = 0
-# for i in aux:
-#     num = arvore.search(int(i))
-#     if maior < num:
-#         maior = num
-# print(maior)
-# print("######################################################")
-# print("######################################################")
+print(arvore.height(arvore.root))
 
 
+print(arvore.search(101))
+arvore.remover(101)
 
-# # print(arvore.search(100))
+# print(arvore.height(arvore.root))
+
+
+# print(arvore.height(arvore.root))
+
+# print(arvore.search(100))
 # for j in comands:
 #     j = j.split(" ")
 #     if j[0] == 'SCH':
@@ -264,18 +287,14 @@ print(arvore.deepSearch())
 #     elif j[0] == "DEL":
 #         print((arvore.search(int(j[1]))))
 #         arvore.remove(int(j[1]))
-# print("######################################################")
-# print("######################################################")
+print("######################################################")
+print("######################################################")
+print(arvore.height(arvore.root))
 
 
-# # arvore.remove(22)
-# # print(arvore.search(22))
-# maior = 0
-# for i in aux:
-#     num = arvore.search(int(i))
-#     if maior < num:
-#         maior = num
-# print(maior)
+
+# arvore.remove(22)
+# print(arvore.search(22))
 
 
 
@@ -309,13 +328,7 @@ def main():
     for i in aux:
         arvore.insert(int(i), i)
 
-
-    maior = 0
-    for i in aux:
-        num = arvore.search(int(i))
-        if maior < num:
-            maior = num
-    print(maior)
+    print(arvore.deepSearch())
 
     for i in lista:
         if i[:3:] == "SCH":
@@ -327,12 +340,7 @@ def main():
             print((arvore.search(int(i[4::]))))
             arvore.remove(int(i[4::]))
     
-    number = 0
-    for j in aux:
-        num = arvore.search(int(j))
-        if number < num:
-            number = num
-    print(number)
+    print(arvore.deepSearch())
 
 if __name__ == '__main__':
     main()
