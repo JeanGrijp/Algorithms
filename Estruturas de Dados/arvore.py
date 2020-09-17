b3d9e3cb466607a8fcde52be1c2502131985c5eb
Jean Grijp <grijpjean@gmail.com>
2020-09-16 21:10:16 -0300

    .

--- Estruturas de Dados/arvore.py
+++ Estruturas de Dados/arvore.py
@@ -259,6 +259,104 @@ class Tree:
         return noAtual.value
 
 
+    def nosucessor(self, apaga): # O parametro é a referencia para o No que deseja-se apagar
+        paidosucessor = apaga
+        sucessor = apaga
+        atual = apaga.dir # vai para a subarvore a direita
+
+        while atual != None: # enquanto nao chegar no Nó mais a esquerda
+            paidosucessor = sucessor
+            sucessor = atual
+            atual = atual.esq # caminha para a esquerda
+
+        # *********************************************************************************
+        # quando sair do while "sucessor" será o Nó mais a esquerda da subarvore a direita
+        # "paidosucessor" será o o pai de sucessor e "apaga" o Nó que deverá ser eliminado
+        # *********************************************************************************
+        if sucessor != apaga.dir: # se sucessor nao é o filho a direita do Nó que deverá ser eliminado
+            paidosucessor.esq = sucessor.dir # pai herda os filhos do sucessor que sempre serão a direita
+            # lembrando que o sucessor nunca poderá ter filhos a esquerda, pois, ele sempre será o
+            # Nó mais a esquerda da subarvore a direita do Nó apaga.
+            # lembrando também que sucessor sempre será o filho a esquerda do pai
+            sucessor.dir = apaga.dir # guardando a referencia a direita do sucessor para 
+                                    # quando ele assumir a posição correta na arvore
+        return sucessor
+
+    def remover(self, v):
+        if self.root == None:
+            return False # se arvore vazia
+        atual = self.root
+        pai = self.root
+        filho_esq = True
+        # ****** Buscando o valor **********
+        while atual.item != v: # enquanto nao encontrou
+            pai = atual
+            if v < atual.item: # caminha para esquerda
+                atual = atual.esq
+                filho_esq = True # é filho a esquerda? sim
+            else: # caminha para direita
+                atual = atual.dir 
+                filho_esq = False # é filho a esquerda? NAO
+            if atual == None:
+                return False # encontrou uma folha -> sai
+        # fim laço while de busca do valor
+
+        # **************************************************************
+        # se chegou aqui quer dizer que encontrou o valor (v)
+        # "atual": contem a referencia ao No a ser eliminado
+        # "pai": contem a referencia para o pai do No a ser eliminado
+        # "filho_esq": é verdadeiro se atual é filho a esquerda do pai
+        # **************************************************************
+
+        # Se nao possui nenhum filho (é uma folha), elimine-o
+        if atual.esq == None and atual.dir == None:
+            if atual == self.root:
+                self.root = None # se raiz
+            else:
+                if filho_esq:
+                        pai.esq =  None # se for filho a esquerda do pai
+                else:
+                        pai.dir = None # se for filho a direita do pai
+
+        # Se é pai e nao possui um filho a direita, substitui pela subarvore a direita
+        elif atual.dir == None:
+            if atual == self.root:
+                self.root = atual.esq # se raiz
+            else:
+                if filho_esq:
+                        pai.esq = atual.esq # se for filho a esquerda do pai
+                else:
+                        pai.dir = atual.esq # se for filho a direita do pai
+        
+        # Se é pai e nao possui um filho a esquerda, substitui pela subarvore a esquerda
+        elif atual.esq == None:
+            if atual == self.root:
+                self.root = atual.dir # se raiz
+            else:
+                if filho_esq:
+                        pai.esq = atual.dir # se for filho a esquerda do pai
+                else:
+                        pai.dir = atual.dir # se for  filho a direita do pai
+
+        # Se possui mais de um filho, se for um avô ou outro grau maior de parentesco
+        else:
+            sucessor = self.nosucessor(atual)
+            # Usando sucessor que seria o Nó mais a esquerda da subarvore a direita do No que deseja-se remover
+            if atual == self.root:
+                self.root = sucessor # se raiz
+            else:
+                if filho_esq:
+                        pai.esq = sucessor # se for filho a esquerda do pai
+                else:
+                        pai.dir = sucessor # se for filho a direita do pai
+            sucessor.esq = atual.esq # acertando o ponteiro a esquerda do sucessor agora que ele assumiu 
+                                    # a posição correta na arvore   
+
+        return True
+
+
+
+
     def pre_ordem(self, no):
         if no is None:
             return
